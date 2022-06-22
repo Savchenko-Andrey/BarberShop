bdf8ec5ef443d88b7476c7a78d7e00e590af1145
Savchenko-Andrey <95830161+Savchenko-Andrey@users.noreply.github.com>
2022-06-22 12:21:38 +0300

    change4

---
+++ js/autoTest.js
@@ -0,0 +1,21 @@
+// function findLongestWord(string) {
+//   // Change code below this line
+
+//   let word = "";
+
+//   for (let i = 0; i < string.split(" ").length; i += 1) {
+//     if (word.length < string.split(" ")[i].length) {
+//       word = string.split(" ")[i];
+//     }
+//   }
+
+//   console.log(word);
+
+//   return word;
+
+//   // Change code above this line
+// }
+// findLongestWord("The quick brown fox jumped over the lazy dog");
+// findLongestWord("May the force be with you");
+// findLongestWord("Google do a roll");
+// findLongestWord();

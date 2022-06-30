620bc8bbb31759d8757ddf101bac24b0f2518da8
Savchenko-Andrey <95830161+Savchenko-Andrey@users.noreply.github.com>
2022-06-30 18:21:22 +0300

    change5

--- js/autoTest.js
+++ js/autoTest.js
@@ -1,21 +1,93 @@
-// function findLongestWord(string) {
-//   // Change code below this line
+// const doMath = function (a, b, callbeck) {
+//   const result = callbeck(a, b);
 
-//   let word = "";
+//   console.log(result);
+// };
 
-//   for (let i = 0; i < string.split(" ").length; i += 1) {
-//     if (word.length < string.split(" ")[i].length) {
-//       word = string.split(" ")[i];
+// doMath(5, 10, function (x, y) {
+//   return x + y;
+// });
+// doMath(900, 8, function (x, y) {
+//   return x * y;
+// });
+// doMath(150, 4, function (x, y) {
+//   return x % y;
+// });
+// doMath(235, 60, function (x, y) {
+//   return x - y;
+// });
+
+// --------- координаты -----------
+
+// const onGetPositionSuccess = function (position) {
+//   console.log("это вызов onGetPositionSuccess");
+//   console.log(position);
+// };
+
+// const onGetPositionError = function (error) {
+//   console.log(error);
+// };
+
+// window.navigator.geolocation.getCurrentPosition(
+//   onGetPositionSuccess,
+//   onGetPositionError
+// );
+
+// // --- отложенный вызов ---
+
+// const callbeck = function () {
+//   console.log("через указаное время сработает вызов");
+// };
+
+// setTimeout(callbeck, 2000);
+
+// // ------ фильтр ------
+
+// const filter = function (array, test) {
+//   const filterAdArray = [];
+
+//   for (const el of array) {
+//     console.log(el);
+//     const passed = test(el);
+
+//     if (passed) {
+//       filterAdArray.push(el);
 //     }
 //   }
 
-//   console.log(word);
+//   return filterAdArray;
+// };
 
-//   return word;
+// const callbeck1 = function (value) {
+//   return value >= 3;
+// };
 
-//   // Change code above this line
-// }
-// findLongestWord("The quick brown fox jumped over the lazy dog");
-// findLongestWord("May the force be with you");
-// findLongestWord("Google do a roll");
-// findLongestWord();
+// const callbeck2 = function (value) {
+//   return value <= 4;
+// };
+
+// const r1 = filter([1, 2, 3, 4, 5], callbeck1);
+// console.log(r1);
+
+// const r2 = filter([1, 2, 3, 4, 5, 6, 7, 8], callbeck2);
+// console.log(r2);
+
+// ------- замыкание --------
+
+// const fnA = function (parametr) {
+//   const innerVariable = "значение внутренней переменной функции fnA";
+
+//   const innerFunction = function () {
+//     console.log(parametr);
+//     console.log(innerVariable);
+//     console.log("это вызов innerFunction");
+//   };
+
+//   return innerFunction;
+// };
+
+// const fnB = fnA(555);
+
+// fnB();
+
+// console.log(fnB);
